cc147119102d46e3a877a7c89e396f85674ccc06
camonday <arkadiakowalczyk@gmail.com>
2022-05-13 12:59:08 +0200

    graf jest losowany (nie jest sprawdzone dla skierowanego) trzeba zrobic by mozna bylo dwa razy wylosowac w tej samej sesji

--- Grafy.cpp
+++ Grafy.cpp
@@ -10,6 +10,7 @@
 #include <fstream>
 #include <chrono>
 #include <list>
+#define max_weight 100
 
 
 using namespace std;
@@ -40,7 +41,7 @@ public:
 private:
     void clear();
     void create();
-    void addConection(int A, int B, int waga);
+    bool addConection(int A, int B, int waga);
     void showList();
     void showMatrix();
     void runAList();
@@ -49,6 +50,9 @@ private:
     void runBMatrix();
     void makeCopyList(list<sasiad>);
     void deleteRandConnection();
+    void addRandConnection(bool tryb);
+    bool czyZepsuje(int elem1, int elem2);
+    int losujWierzcholek() const;
 
 
 };
@@ -97,7 +101,8 @@ void Graph::create() {
     }
 }
 
-void Graph::addConection(int A, int B, int waga) {
+bool Graph::addConection(int A, int B, int waga) {  //zwraca false gdy nie uda sie dodac krawedzi
+    if(macierz[A][B]!=0) return false;
     //lista
     listaSasiedztwa[A].push_front(sasiad{B,waga});
     //macierz
@@ -107,6 +112,8 @@ void Graph::addConection(int A, int B, int waga) {
         listaSasiedztwa[B].push_front(sasiad{A,waga});
         macierz[B][A] = waga;
     }
+    //show();
+    return true;
 }
 
 void Graph::showList() {
@@ -150,31 +157,136 @@ void Graph::showMatrix() {
 }
 
 void Graph::generate(int wierz, int gestosc, bool czySkierowany) {
-    this->czySkierowany=czySkierowany;
-    wierzcholki=wierz;
-    krawedzie=wierzcholki*wierzcholki;
-    krawedzie-=wierzcholki;
-    krawedzie*=(gestosc/200);
-    int istKraw = 0;
+    clear();
+    this->czySkierowany = czySkierowany;
+    wierzcholki = wierz;
+    krawedzie = wierzcholki * wierzcholki;      // realizacja rownania:gestosc % = 2*krawedzie/(wierzcholki*wierzcholki - wierzcholki)
+    krawedzie -= wierzcholki;                   // maks ilosc wierzcholkow
+    krawedzie *= gestosc;                       // krawedzie = (w*w - w) * g / 2*100
+    krawedzie /= 200;
+    if(czySkierowany) krawedzie*=2;             // w grafie skierowanym moze byc 2 razy wiecej krawedzi
 
-    create();
-    //tworzenie drzewa rozpinającego
+    int istKraw = 0;                // potrzebne do sprawdzenia, czy mamy 99%
+    create();                       //mamy wierzcholki, nie mamy krawedzi
+
+    //TWORZENIE DRZEWA ROZPINAJACEGO
     //1. utworz liste wierzcholkow ktorych nie ma w grafie
+    list<int> list1;
+    for (int i = 0; i < wierzcholki; ++i) list1.push_front(i);
+    int elem1;
 
     //2. utworz pusta liste
+    list<int> list2;
+    int elem2;
+
+    _List_const_iterator<int> adr;
+    int waga;
 
     //3. pierwszy wierzchołek to 0, dodaj go do listy tych ktore sa w grafie
+    list2.push_front(list1.back());
+    list1.pop_back();
+
 
     //4. dopuki lista1 nie jest pusta:
+    while (!list1.empty()) {
+        cout<<"\nszukam";
         //4a. wylosuj wierzcholek z listy2
+        adr=list2.cbegin();
+        for(int i = rand()%list2.size(); i>0; i--) adr.operator++();  //pamietaj o lepszym rand
+        elem2 = *adr;
         //4b. wylosuj wierzcholek z listy 1, usun go z listy 1 i dodaj do listy 2
-        //4c. wylosuj wage
-        //4d. utworz polaczenie i zwieksz istniejace kraw
+        adr = list1.cbegin();
+        for(int i = rand()%list1.size(); i>0; i--) ++adr;  // jw
+        elem1 = *adr;
 
-    //5. wyczysc listy
+        list1.remove(elem1);
+        list2.push_back(elem1);
+        //4c. wylosuj wage
+        waga = rand()%max_weight;                               //jw
+        //4d. utworz polaczenie i zwieksz istniejace kraw
+        addConection(elem2, elem1, waga);
+        istKraw++;
+        cout<<"\n rozpinajajcy "<<list1.size();
+        //showList();
+        //getch();
+    }
     //6. jesli to graf skierowany to do wierz 0 dodaj krawedz tak, by 0 był celem
+    if(czySkierowany){
+        addRandConnection(true);
+        istKraw++;
+    }
+
+
     //dodaj pozostałe krawedzie
+    while(istKraw<krawedzie){
+        addRandConnection(false);
+        istKraw++;
+    }
     // if 99 gest - dodaj dodatkowe 1/99 krawedzi i usun losowe 1/99 krawedzi
+    if(gestosc==99){
+        for (int i = istKraw/99; i >0 ; --i) {
+            addRandConnection(false);
+            istKraw++;
+        }
+        while(istKraw!=krawedzie){
+            deleteRandConnection();
+            istKraw--;
+        }
+    }
+}
+
+void Graph::addRandConnection(bool tryb) {
+    int elem1, elem2, waga;
+    do {
+        elem1 = losujWierzcholek();
+        if (tryb) elem1 = 0;
+        do {
+            elem2 = losujWierzcholek();
+        } while (elem1 == elem2);
+        waga = rand() % max_weight;
+    } while(!addConection(elem2, elem1, waga));
+}
+
+void Graph::deleteRandConnection() {
+    int elem1, elem2;
+    do{                                         //1. Wylosuj
+        do{
+            elem1=losujWierzcholek();
+        } while (listaSasiedztwa[elem1].size()<2);
+        do{
+            elem2=listaSasiedztwa[elem1].front().id;
+            listaSasiedztwa[elem1].push_back(listaSasiedztwa[elem1].front());
+            listaSasiedztwa[elem1].pop_front();
+        } while (macierz[elem1][elem2]==0);
+    } while(czyZepsuje(elem1, elem2));            //2. Sprawdz czy nie zaburzysz grafu
+    //3. usun
+    listaSasiedztwa[elem1].pop_back();
+    macierz[elem1][elem2]=0;
+    if(!czySkierowany){
+       for(sasiad i = listaSasiedztwa[elem2].front(); i.id!=elem1; ){
+           listaSasiedztwa[elem2].push_back(listaSasiedztwa[elem2].front());
+           listaSasiedztwa[elem2].pop_front();
+       }
+       listaSasiedztwa[elem2].pop_back();
+       macierz[elem2][elem1]=0;
+    }
+}
+
+bool Graph::czyZepsuje(int elem1, int elem2) {
+    //1 zsumuj wagi wiersza elem 2
+    int suma = 0;
+    for(int i=0; i<wierzcholki; i++){
+        suma += macierz[i][elem2];
+    }
+    //2 odejmij od tego wage elem1:elem2
+    suma -= macierz[elem1][elem2];
+    //3 sprawdz czy ostrzymales wiecej niz 0
+    if(suma==0) return true;
+    return false;
+}
+
+int Graph::losujWierzcholek() const {
+    return rand()%wierzcholki;
 }
 
 
@@ -200,7 +312,15 @@ void loadGraph(bool czySkierowany){
     myGraph.loadFromFile(fileName,czySkierowany);
     myGraph.show();
 }
-void createGraph(){}
+void createGraph(bool czySkierowany){
+    int wierz, gest;
+    cout<<"\nPodaj liczbe wierzcholkow: ";
+    cin>>wierz;
+    cout<<"\nPodaj gestosc: ";
+    cin>>gest;
+    myGraph.generate(wierz, gest, czySkierowany);
+    myGraph.show();
+}
 void showGraph(){}
 
 void menu_a() {
@@ -217,7 +337,7 @@ void menu_a() {
                 break;
 
             case '2': //tutaj losowanie
-                createGraph();
+                createGraph(0);
                 break;
 
             case '3': //tutaj wyswietlenie
@@ -234,6 +354,7 @@ void menu_a() {
 
         }
     } while (opt != 0);
+    return;
 }
 void menu_b() {
     char opt;
@@ -249,7 +370,7 @@ void menu_b() {
                 break;
 
             case '2': //tutaj losowanie
-                createGraph();
+                createGraph(1);
                 break;
 
             case '3': //tutaj wyswietlenie
